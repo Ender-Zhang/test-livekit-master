9f47612840b37af23cccfdac1eafcee10433499d
Ender-Zhang <102596313+Ender-Zhang@users.noreply.github.com>
2023-06-23 12:13:40 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@
  * @Author: Ender-Zhang 102596313+Ender-Zhang@users.noreply.github.com
  * @Date: 2023-05-15 09:35:40
  * @LastEditors: Ender-Zhang 102596313+Ender-Zhang@users.noreply.github.com
- * @LastEditTime: 2023-06-18 14:57:14
+ * @LastEditTime: 2023-06-21 20:18:37
  * @FilePath: \test-livekit-master\App.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -111,7 +111,7 @@ function HomeScreen({ navigation }: any) {
     fetch("http://10.0.2.2:8080/api/patient-login", requestOptions)
       .then(response => response.json())
       .then((result) => { 
-        console.log("result:",result);
+        // console.log("result:",result);
         // login successfully
         navigation.navigate('LoadingPage', {
           userId: userId,
